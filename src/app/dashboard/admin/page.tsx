ab494cbb43d78872f3e80beab748a34e2edf3993
Nev <ngushteraklieva@gmail.com>
2025-09-09 09:34:40 +0200

    structure admin page and add responsive design

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,6 +1,11 @@
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex flex-col gap-4 md:flex-row'>
+      {/* Left */}
+      <div className="w-full lg:w-2/3"></div>
+      {/* Right */}
+      <div className="w-full lg:w-1/3"></div>
+    </div>
   )
 }
 
