3f527774311ab4724e03dd88d9803da47f050106
Nev <ngushteraklieva@gmail.com>
2025-09-09 09:49:59 +0200

    Use UserCard in the admin page

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,8 +1,18 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
     <div className='p-4 flex flex-col gap-4 md:flex-row'>
       {/* Left */}
-      <div className="w-full lg:w-2/3"></div>
+      <div className="w-full lg:w-2/3">
+      {/* UserCard */}
+      <div className="flex gap-4 justify-between">
+        <UserCard type="student" />
+        <UserCard type="parent" />
+        <UserCard type="teacher" />
+        <UserCard type="staff" />
+      </div>
+      </div>
       {/* Right */}
       <div className="w-full lg:w-1/3"></div>
     </div>
