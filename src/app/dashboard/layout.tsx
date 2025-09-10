5a1cbb83b2cc912ccb98796c526480f630951ff5
Nev <ngushteraklieva@gmail.com>
2025-09-10 14:05:30 +0200

    Style the Icon

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
     <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-green-200 p-4">
         <Link href="/dashboard" className="flex justtify-center items-center lg:justify-start gap-2">
         <Image src="/logo.png" alt="logo" width={32} height={32}/>
-        <span className="hidden lg:block">SchoolLama</span>
+        <span className="hidden lg:block font-bold">SchoolLama</span>
         </Link>
         <Menu/>
     </div>
